0adf0dae2739c6ef0e0963f71ab8d865eeb2fad8
RickDvn <168721035+RickDvn@users.noreply.github.com>
2024-05-08 17:07:43 +0200

    Creacion del HeaderWriter para el CSV

---
+++ src/main/java/com/viewnext/batchdatabase/batch/writer/TarifaHeaderWriter.java
@@ -0,0 +1,20 @@
+package com.viewnext.batchdatabase.batch.writer;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import org.springframework.batch.item.file.FlatFileHeaderCallback;
+
+public class TarifaHeaderWriter implements FlatFileHeaderCallback {
+
+	private final String header;
+
+	TarifaHeaderWriter(String header) {
+        this.header = header;
+    }
+
+	@Override
+    public void writeHeader(Writer writer) throws IOException {
+        writer.write(header);
+    }
+}
